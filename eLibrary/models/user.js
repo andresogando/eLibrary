7714e2b1ee2e94ad3e2c20bd67a88f3eff0f19a8
andress <Andres@Andress-MacBook-Pro.local>
2019-12-02 23:09:48 -0400

    added email to the user schema

--- eLibrary/models/user.js
+++ eLibrary/models/user.js
@@ -6,7 +6,9 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
     username: String, 
-    password: String
+    password: String,
+    email: String,
+    isAdmin: {type:Boolean, default: false}
 });
 
 UserSchema.plugin(passportLocalMongoose);
--- eLibrary/routes/index.js
+++ eLibrary/routes/index.js
@@ -17,7 +17,7 @@ router.get("/register", function(req, res){
 //handle sign up logic
 router.post("/register",function(req, res){
     var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
+    User.register(newUser, req.body.password, req.body.email, function(err, user){
         if(err){
             console.log(err);
             req.flash("error", err.message)
